882944dea6e702ab88fb1085a38f193b3f576be4
DonkyKing69 <83776385+DonkyKing69@users.noreply.github.com>
2021-05-07 07:20:55 +0600

    Update AvitoParser.py

--- AvitoParser.py
+++ AvitoParser.py
@@ -12,7 +12,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 domain = domain['domain']
 
 def getHtml(url):
-    return requests.get(url, headers=headers, proxies=proxies).text   # TODO Если нужно использовать прокси
+    return requests.get(url, headers=headers, proxies={"http": "http://1mqtydna:u3cu3eye@194.39.101.70:15652"}).text   # TODO Если нужно использовать прокси
 #     return requests.get(url, headers=headers).text
 
 def parse(html):
